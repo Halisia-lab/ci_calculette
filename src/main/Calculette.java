4ddd94c2881c773d1a982e499e7166a3b236cc30
Halisia <halifa.halisia@gmail.com>
2021-06-07 17:56:18 +0200

    add code

---
+++ src/main/Calculette.java
@@ -0,0 +1,38 @@
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Calculette {
+
+
+    public static int add(int a, int b) {
+        return a+b;
+    }
+
+    public static int sub(int a, int b) {
+        return a-b;
+    }
+
+    public static int mul(int a, int b) {
+        return a*b;
+    }
+
+    public static double div (int a, int b) throws Exception {
+        if(b != 0) {
+            return a/b;
+        } else {
+            throw new Exception("Division par zero");
+        }
+    }
+
+    public static double avg(int[] tab) throws Exception {
+        if(tab.length != 0) {
+            int total = 0;
+            for(int number: tab) {
+                total += number;
+            }
+            return total/(tab.length);
+        } else {
+            throw new Exception("Liste vide");
+        }
+
+    }
+}
---
+++ src/test/CalculetteTest.java
@@ -0,0 +1,58 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CalculetteTest {
+    @Test
+    public void testCalculetteAdd() {
+        Assert.assertEquals(4, Calculette.add(2,2));
+    }
+
+
+    @Test
+    public void testCalculetteSub() {
+        Assert.assertEquals(0, Calculette.sub(2,2));
+        Assert.assertEquals(-1, Calculette.sub(2,3));
+    }
+
+    @Test
+    public void testCalculetteMul() {
+        Assert.assertEquals(9, Calculette.mul(3,3));
+        Assert.assertEquals(-6, Calculette.mul(2,-3));
+    }
+
+    @Test
+    public void testCalculetteDiv() {
+        try {
+            Assert.assertEquals(3, Calculette.div(9, 3), 0.01);
+            Assert.assertEquals(0, Calculette.div(5, 0), 0.01);
+        }
+        catch (Exception e) {
+            final String expected = "Division par zero";
+            Assert.assertEquals( expected, e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCalculetteAvg() {
+        int[] list = {0,3,6};
+        int[] list2 = {};
+
+        try {
+            Assert.assertEquals(3, Calculette.avg(list), 0.01);
+            Assert.assertEquals(0, Calculette.avg(list2), 0.01);
+        }
+        catch (Exception e) {
+            final String expected = "Liste vide";
+            Assert.assertEquals( expected, e.getMessage());
+        }
+    }
+
+    //CORRECTION
+
+
+
+}
